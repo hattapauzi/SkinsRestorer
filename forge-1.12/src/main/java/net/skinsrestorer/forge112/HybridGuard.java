c529adcb5deaac093780d351b6b0b9ce22460d71
hattapauzi <hattapauzi@proton.me>
2026-09-21 16:43:02 +0800

    feat: refuse to start companion when Bukkit is on the classpath

---
+++ forge-1.12/src/main/java/net/skinsrestorer/forge112/HybridGuard.java
@@ -0,0 +1,32 @@
+/*
+ * SkinsRestorer
+ * Copyright (C) 2026  SkinsRestorer Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package net.skinsrestorer.forge112;
+
+public final class HybridGuard {
+    public static boolean isBukkitPresent() {
+        try {
+            Class.forName("org.bukkit.Bukkit");
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
+
+    private HybridGuard() {
+    }
+}
