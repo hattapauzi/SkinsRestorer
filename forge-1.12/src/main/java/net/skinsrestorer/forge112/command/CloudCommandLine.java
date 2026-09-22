20c8b7c7e84b2e845eda97bd951cfe4d27886149
hattapauzi <hattapauzi@proton.me>
2026-09-23 00:58:56 +0800

    feat: build Cloud command lines from Forge ICommand args

---
+++ forge-1.12/src/main/java/net/skinsrestorer/forge112/command/CloudCommandLine.java
@@ -0,0 +1,30 @@
+/*
+ * SkinsRestorer
+ * Copyright (C) 2026  SkinsRestorer Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package net.skinsrestorer.forge112.command;
+
+public final class CloudCommandLine {
+    public static String line(String name, String[] args) {
+        if (args == null || args.length == 0) {
+            return name;
+        }
+        return name + " " + String.join(" ", args);
+    }
+
+    private CloudCommandLine() {
+    }
+}
---
+++ forge-1.12/src/test/java/net/skinsrestorer/forge112/command/CloudForgeCommandLineTest.java
@@ -0,0 +1,34 @@
+/*
+ * SkinsRestorer
+ * Copyright (C) 2026  SkinsRestorer Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package net.skinsrestorer.forge112.command;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CloudForgeCommandLineTest {
+    @Test
+    void rootOnly() {
+        assertEquals("skin", CloudCommandLine.line("skin", new String[0]));
+    }
+
+    @Test
+    void joinsArgs() {
+        assertEquals("skin set Notch", CloudCommandLine.line("skin", new String[]{"set", "Notch"}));
+    }
+}
